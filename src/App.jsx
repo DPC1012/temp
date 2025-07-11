caea8513e87ede73bd9bb19e995197a4aba7b517
devarsh <devarshpc@gmail.com>
2025-07-11 14:59:52 +0530

    Added count button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
@@ -6,10 +7,23 @@ function App() {
     <>
       <div  >
         hi there
-        <button className=''>hello</button>
+      </div>
+      <div>
+        <Temp />
       </div>
     </>
   )
+}
+
+function Temp(){
+  const [count , setCount] = useState(0)
+  function handle()
+  {
+      setCount(count+1)
+  }
+  return <div >
+  <button className='bg-black text-white rounded-full px-2 cursor-pointer' onClick={handle} >Count {count}</button>
+  </div>
 }
 
 export default App
